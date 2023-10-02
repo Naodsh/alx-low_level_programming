f699dac4d01decfdf788d1147f3bf23e6d8df445
Naodsh <naodadanech1977@gmail.com>
2023-10-02 13:44:44 -0700

    third task correction

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -24,7 +24,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		write_result = write(fd, text_content, strlen(text_content));
-		fd = (write_result == -1)
+		if (write_result == -1)
 		{
 			close(fd);
 			return (-1);
